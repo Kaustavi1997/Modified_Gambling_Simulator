1fb13a82d4df717a9fd0cbd0d69988c06c7e271d
“Kaustavi <kaustavisen8@gmail.com>
2020-06-09 19:20:13 +0530

    Added:Simulation for one bet

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -3,4 +3,20 @@
 STAKE_PER_DAY=100
 BET_PER_GAME=1
 
+resultPerGame=-1
+simulateOneGame(){
+	WIN=1
+	LOSE=0
+
+	check=$((RANDOM%2))
+	if [ $check -eq $WIN ];
+	then
+		resultPerGame=$BET_PER_GAME
+	else
+		resultPerGame=-$BET_PER_GAME
+	fi
+}
+simulateOneGame
+echo $resultPerGame
+
 
